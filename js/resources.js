73a986b7b475c3ea4c18f89c0e0ec636c0ab012d
Julian van Riet <vanrietjulian@gmail.com>
2015-01-22 08:42:47 -0800

    Initial Push

---
+++ js/resources.js
@@ -0,0 +1,31 @@
+game.resources = [
+
+	/* Graphics. 
+	 * @example
+	 * {name: "example", type:"image", src: "data/img/example.png"},
+	 */
+	{name: "background-tiles", type:"image", src: "data/img/background-tiles.png"},
+	{name: "meta-tiles2", type:"image", src: "data/img/meta-tiles2.png"},
+
+	/* Atlases 
+	 * @example
+	 * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
+	 */
+		
+	/* Maps. 
+	 * @example
+	 * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
+	 * {name: "example01", type: "tmx", src: "data/map/example01.json"},
+ 	 */
+ 	{name: "map1", type: "tmx", src: "data/map/map1.tmx"},
+
+	/* Background music. 
+	 * @example
+	 * {name: "example_bgm", type: "audio", src: "data/bgm/"},
+	 */	
+
+	/* Sound effects. 
+	 * @example
+	 * {name: "example_sfx", type: "audio", src: "data/sfx/"}
+	 */
+];
