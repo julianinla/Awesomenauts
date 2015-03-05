3d08680201d89ed49de42fb5c37beb6ba433a1e7
Julian van Riet <vanrietjulian@gmail.com>
2015-03-05 08:56:21 -0800

    Start New

--- js/screens/title.js
+++ js/screens/title.js
@@ -6,35 +6,37 @@ game.TitleScreen = me.ScreenObject.extend({
 		me.game.world.addChild(new me.Sprite(0, 0, me.loader.getImage('title-screen')), -10); 
 		//added a title screen image that needs to load
 
-		me.input.bindKey(me.input.KEY.ENTER, "start");
-		//binding key so can go to playscreen from title screen
-
 		me.game.world.addChild(new (me.Renderable.extend({
 			init: function() {
-				this._super(me.Renderable, 'init', [510, 30, me.game.viewport.width, me.game.viewport.height]);
+				this._super(me.Renderable, 'init', [270, 240, 300, 50]);
 				//basic settings for the title screen
 				this.font = new me.Font("Arial", 46, "white");
 				//font used in title
+				me.input.registerPointerEvent('pointerdown', this, this.newGame.bind(this), true);
 			},
 
 			draw: function(renderer) {
 				this.font.draw(renderer.getContext(), "Awesomenauts!", 325, 130);
+				this.font.draw(renderer.getContext(), "START A NEW GAME", this.pos.x, this.pos.y);
 				//draw a awesomenauts title
-				this.font.draw(renderer.getContext(), "Press ENTER to Play!", 250, 530);
-				//draw a queue for starting game
-			}
+			},
 			//used as main function to draw on screen
+
+			update: function(dt) {
+				return true;
+			},
+
+			newGame: function() {
+				me.input.releasePointerEvent('pointerdown', this);
+				me.save.remove('exp');
+				me.save.remove('exp1');
+				me.save.remove('exp2');
+				me.save.remove('exp3');
+				me.save.remove('exp4');
+				me.state.change(me.state.PLAY);
+			}
 		})));
 		//generic renderable
-
-		this.handler = me.event.subscribe(me.event.KEYDOWN, function(action, keyCode, edge) {
-			if(action === "start") {
-				me.state.change(me.state.PLAY);
-				//go to the playscreen
-			}
-			//if enter key setup is pressed
-		});
-		//event handler for pressing enter key
 
 	},
 	
@@ -43,7 +45,6 @@ game.TitleScreen = me.ScreenObject.extend({
 	 *  action to perform when leaving this screen (state change)
 	 */
 	onDestroyEvent: function() {
-		me.input.unbindKey(me.input.KEY.ENTER);
-		me.event.unsubscribe(this.handler);
+
 	}
 });
