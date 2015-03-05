b6ca220a73b0f1f267f7fa7e7d80780eb96da39c
Julian van Riet <vanrietjulian@gmail.com>
2015-03-05 09:08:27 -0800

    Continue

--- js/screens/title.js
+++ js/screens/title.js
@@ -17,8 +17,6 @@ game.TitleScreen = me.ScreenObject.extend({
 			},
 
 			draw: function(renderer) {
-				this.font.draw(renderer.getContext(), "Awesomenauts!", 325, 130);
-				//gives game title on start screen
 				this.font.draw(renderer.getContext(), "START A NEW GAME", this.pos.x, this.pos.y);
 				//draw new game start
 			},
@@ -42,6 +40,35 @@ game.TitleScreen = me.ScreenObject.extend({
 			}
 		})));
 		//generic renderable
+
+		me.game.world.addChild(new (me.Renderable.extend({
+			init: function() {
+				this._super(me.Renderable, 'init', [380, 340, 300, 50]);
+				//basic settings for the title screen
+				this.font = new me.Font("Arial", 46, "white");
+				//font used in title
+				me.input.registerPointerEvent('pointerdown', this, this.newGame.bind(this), true);
+				//handles being able to click on start
+			},
+
+			draw: function(renderer) {
+				this.font.draw(renderer.getContext(), "CONTINUE", this.pos.x, this.pos.y);
+				//draw new game start
+			},
+			//used as main function to draw on screen
+
+			update: function(dt) {
+				return true;
+			},
+
+			newGame: function() {
+				me.input.releasePointerEvent('pointerdown', this);
+				//gets rid of pointerfunction
+				me.state.change(me.state.PLAY);
+				//starts the game
+			}
+		})));
+		//renderable
 
 	},
 	
