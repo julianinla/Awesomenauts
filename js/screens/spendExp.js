4ccca38c83c48365b5b63dd142d3b15215d1f92a
Julian van Riet <vanrietjulian@gmail.com>
2015-03-11 22:18:22 -0700

    Finishing SpendExp Screen w/ Comments

--- js/screens/spendExp.js
+++ js/screens/spendExp.js
@@ -6,11 +6,11 @@ game.SpendExp = me.ScreenObject.extend({
 		me.game.world.addChild(new me.Sprite(0, 0, me.loader.getImage('exp-screen')), -10); 
 		//added a exp screen image that needs to load
 
-		me.input.bindKey(me.input.KEY.F1, "F1");
-		me.input.bindKey(me.input.KEY.F2, "F2");
-		me.input.bindKey(me.input.KEY.F3, "F3");
-		me.input.bindKey(me.input.KEY.F4, "F4");
-		me.input.bindKey(me.input.KEY.F5, "F5");
+		me.input.bindKey(me.input.KEY.F1, "F1"); //initializes f1 key
+		me.input.bindKey(me.input.KEY.F2, "F2"); //initializes f2 key
+		me.input.bindKey(me.input.KEY.F3, "F3"); //initializes f3 key
+		me.input.bindKey(me.input.KEY.F4, "F4"); //initializes f4 key
+		me.input.bindKey(me.input.KEY.F5, "F5"); //initializes f5 key
 
 		me.game.world.addChild(new (me.Renderable.extend({
 			init: function() {
@@ -27,14 +27,14 @@ game.SpendExp = me.ScreenObject.extend({
 				//draws current exp text
 				this.font.draw(renderer.getContext(), "F1: MIDAS CURRENT LEVEL: " + game.data.exp1.toString() + 
 				" COST: " + ((game.data.exp1 + 1) * 10), this.pos.x, this.pos.y + 150);
-				//draws option 1
+				//draws option 1, midas ability if have enough
 				this.font.draw(renderer.getContext(), "F2: ZEPHYR CURRENT LEVEL: " + game.data.exp2.toString() +
 				" COST: " + ((game.data.exp2 + 1) * 10), this.pos.x, this.pos.y + 200);
-				//draws option 2
+				//draws option 2, , zephyr ability if have enough
 				this.font.draw(renderer.getContext(), "F3: ARES ", this.pos.x, this.pos.y + 250);
-				//draws option 3
+				//draws option 3, have to finish ...
 				this.font.draw(renderer.getContext(), "F4: VULCAN ", this.pos.x, this.pos.y + 300);
-				//draws option 4
+				//draws option 4, have to finish ...
 			},
 			//used as main function to draw on screen
 		})));
@@ -42,21 +42,28 @@ game.SpendExp = me.ScreenObject.extend({
 
 		this.handler = me.event.subscribe(me.event.KEYDOWN, function (action, keyCode, edge) {
 			if (action === "F1") {
-
+				//empty for now
 			}
+			//if you press f1
 			else if (action === "F2") {
-
+				//empty for now
 			}
+			//if you press f1
 			else if (action === "F3") {
-				
+				//empty for now
 			}
+			//if you press f1
 			else if (action === "F4") {
-				
+				//empty for now
 			}
+			//if you press f1
 			else if (action === "F5") {
 				me.state.change(me.state.PLAY);
+				//causes game to load, skips exp screen
 			}
+			//if you press f1
 		});
+		//event handler for pressing the f keys
 	},
 	
 	
@@ -64,11 +71,12 @@ game.SpendExp = me.ScreenObject.extend({
 	 *  action to perform when leaving this screen (state change)
 	 */
 	onDestroyEvent: function() {
-		me.input.bindKey(me.input.KEY.F1, "F1");
-		me.input.bindKey(me.input.KEY.F2, "F2");
-		me.input.bindKey(me.input.KEY.F3, "F3");
-		me.input.bindKey(me.input.KEY.F4, "F4");
-		me.input.bindKey(me.input.KEY.F5, "F5");
+		me.input.bindKey(me.input.KEY.F1, "F1"); //gets rid of key bind
+		me.input.bindKey(me.input.KEY.F2, "F2"); //gets rid of key bind
+		me.input.bindKey(me.input.KEY.F3, "F3"); //gets rid of key bind
+		me.input.bindKey(me.input.KEY.F4, "F4"); //gets rid of key bind
+		me.input.bindKey(me.input.KEY.F5, "F5"); //gets rid of key bind
 		me.event.unsubscribe(this.handler);
+		//unsubscribes from event handler, getting rid of it
 	}
 });
