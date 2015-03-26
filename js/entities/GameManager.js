3c541e23a04e6d6da55be29702263bda8aa53f79
Julian van Riet <vanrietjulian@gmail.com>
2015-03-26 09:02:40 -0700

    Awesomenauts 50 Making Purchases

--- js/entities/GameManager.js
+++ js/entities/GameManager.js
@@ -161,6 +161,8 @@ game.SpendGold = Object.extend({
 		}
 		//if hit buy screen
 
+		this.checkBuyKeys();
+
 		return true;
 	},
 	//update function for later
@@ -208,12 +210,12 @@ game.SpendGold = Object.extend({
 			draw: function(renderer) {
 				this.font.draw(renderer.getContext(), "PRESS F1-F6 TO BUY, B TO EXIT, CURRENT GOLD: " + game.data.gold, this.pos.x, this.pos.y);
 				//draw spend gold screen, gives command options
-				this.font.draw(renderer.getContext(), "SKILL 1: CURRENT LEVEL: " + game.data.skill1 + " COSTS: " + ((game.data.skill1 + 10) * 10), this.pos.x, this.pos.y + 40); //skill1 in buyscreen
-				this.font.draw(renderer.getContext(), "SKILL 2: CURRENT LEVEL: " + game.data.skill2 + " COSTS: " + ((game.data.skill2 + 10) * 10), this.pos.x, this.pos.y + 80); //skill2 in buyscreen
-				this.font.draw(renderer.getContext(), "SKILL 3: CURRENT LEVEL: " + game.data.skill3 + " COSTS: " + ((game.data.skill3 + 10) * 10), this.pos.x, this.pos.y + 120); //skill3 in buyscreen
-				this.font.draw(renderer.getContext(), "Q ABILITY: CURRENT LEVEL: " + game.data.ability1 + " COSTS: " + ((game.data.ability1 + 10) * 10), this.pos.x, this.pos.y + 160); //ability1 in buyscreen
-				this.font.draw(renderer.getContext(), "W ABILITY: CURRENT LEVEL: " + game.data.ability2 + " COSTS: " + ((game.data.ability2 + 10) * 10), this.pos.x, this.pos.y + 200); //ability2 in buyscreen
-				this.font.draw(renderer.getContext(), "E ABILITY: CURRENT LEVEL: " + game.data.ability3 + " COSTS: " + ((game.data.ability3 + 10) * 10), this.pos.x, this.pos.y + 240); //ability3 in buyscreen
+				this.font.draw(renderer.getContext(), "SKILL 1: CURRENT LEVEL: " + game.data.skill1 + " COSTS: " + ((game.data.skill1 + 1) * 10), this.pos.x, this.pos.y + 40); //skill1 in buyscreen
+				this.font.draw(renderer.getContext(), "SKILL 2: CURRENT LEVEL: " + game.data.skill2 + " COSTS: " + ((game.data.skill2 + 1) * 10), this.pos.x, this.pos.y + 80); //skill2 in buyscreen
+				this.font.draw(renderer.getContext(), "SKILL 3: CURRENT LEVEL: " + game.data.skill3 + " COSTS: " + ((game.data.skill3 + 1) * 10), this.pos.x, this.pos.y + 120); //skill3 in buyscreen
+				this.font.draw(renderer.getContext(), "Q ABILITY: CURRENT LEVEL: " + game.data.ability1 + " COSTS: " + ((game.data.ability1 + 1) * 10), this.pos.x, this.pos.y + 160); //ability1 in buyscreen
+				this.font.draw(renderer.getContext(), "W ABILITY: CURRENT LEVEL: " + game.data.ability2 + " COSTS: " + ((game.data.ability2 + 1) * 10), this.pos.x, this.pos.y + 200); //ability2 in buyscreen
+				this.font.draw(renderer.getContext(), "E ABILITY: CURRENT LEVEL: " + game.data.ability3 + " COSTS: " + ((game.data.ability3 + 1) * 10), this.pos.x, this.pos.y + 240); //ability3 in buyscreen
 			},
 			//used as main function to draw on screen
 		}));
@@ -237,11 +239,95 @@ game.SpendGold = Object.extend({
 		me.input.unbindKey(me.input.KEY.F3, "F3", true);
 		me.input.unbindKey(me.input.KEY.F4, "F4", true);
 		me.input.unbindKey(me.input.KEY.F5, "F5", true);
-		me.input.unbindKey(me.input.KEY.F6, "F6", true);ee
+		me.input.unbindKey(me.input.KEY.F6, "F6", true);
 		//removes key binds for buyscreen
 		me.game.world.removeChild(game.data.buytext);
 		//removes text after close buyscreen
-	}
+	},
 	//stops the buying screen
+
+	checkBuyKeys: function() {
+		if (me.input.isKeyPressed("F1")) {
+			if(this.checkCost(1)) {
+				this.makePurchase(1);
+			}
+		}
+		else if (me.input.isKeyPressed("F2")) {
+			if(this.checkCost(2)) {
+				this.makePurchase(2);
+			}
+		}
+		else if (me.input.isKeyPressed("F3")) {
+			if(this.checkCost(3)) {
+				this.makePurchase(3);
+			}
+		}
+		else if (me.input.isKeyPressed("F4")) {
+			if(this.checkCost(4)) {
+				this.makePurchase(4);
+			}
+		}
+		else if (me.input.isKeyPressed("F5")) {
+			if(this.checkCost(5)) {
+				this.makePurchase(5);
+			}
+		}
+		else if (me.input.isKeyPressed("F6")) {
+			if(this.checkCost(6)) {
+				this.makePurchase(6);
+			}
+		}
+	},
+
+	checkCost: function(skill) {
+		if (skill === 1 && (game.data.gold >= ((game.data.skill1 + 1) * 10))) {
+			return true;
+		}
+		if (skill === 2 && (game.data.gold >= ((game.data.skill2 + 1) * 10))) {
+			return true;
+		}
+		if (skill === 3 && (game.data.gold >= ((game.data.skill3 + 1) * 10))) {
+			return true;
+		}
+		if (skill === 4 && (game.data.gold >= ((game.data.ability1 + 1) * 10))) {
+			return true;
+		}
+		if (skill === 5 && (game.data.gold >= ((game.data.ability2 + 1) * 10))) {
+			return true;
+		}
+		if (skill === 6 && (game.data.gold >= ((game.data.ability3 + 1) * 10))) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	},
+
+	makePurchase: function(skill) {
+		if (skill === 1) {
+			game.data.gold -= ((game.data.skill1 + 1) * 10);
+			game.data.skill1 += 1;
+		}
+		else if (skill === 2) {
+			game.data.gold -= ((game.data.skill2 + 1) * 10);
+			game.data.skill2 += 1;
+		}
+		else if (skill === 3) {
+			game.data.gold -= ((game.data.skill3 + 1) * 10);
+			game.data.skill3 += 1;
+		}
+		else if (skill === 4) {
+			game.data.gold -= ((game.data.ability1 + 1) * 10);
+			game.data.ability1 += 1;
+		}
+		else if (skill === 5) {
+			game.data.gold -= ((game.data.ability2 + 1) * 10);
+			game.data.ability2 += 1;
+		}
+		else if (skill === 6) {
+			game.data.gold -= ((game.data.ability3 + 1) * 10);
+			game.data.ability3 += 1;
+		}
+	}
 });
 //spend gold manager for handling buying stuff w/ gold
