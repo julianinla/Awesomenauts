3822dfb7e64f2b1895e975c691fec5fdaaf622a3
Julian van Riet <vanrietjulian@gmail.com>
2015-04-04 16:32:58 -0700

    Awesomenauts 58 Jquery Saving w/ Comments

--- js/entities/GameManagers/ExperienceManager.js
+++ js/entities/GameManagers/ExperienceManager.js
@@ -41,33 +41,35 @@ game.ExperienceManager = Object.extend({
 		me.save.exp = game.data.exp;
 		//saves your exp in melonjs
 
-			$.ajax({
-				type: "POST", //type post?
-				url: "php/controller/save-user.php", //url for creating users
-				data: {
-					exp: game.data.exp,
-					exp1: game.data.exp1,
-					exp2: game.data.exp2,
-					exp3: game.data.exp3,
-					exp4: game.data.exp4,
-				},
-				//sets username and password to entered info
-				dataType: "text"
-			})
-			.success(function(response) {
-				if(response === "true") {
-					me.state.change(me.state.PLAY);
-					//start game
-				}
-				else {
-					alert(response);
-				}
-			})
-			//if successful 
-			.fail(function(response) {
-				alert("fail");
-			});
-			//if if fails say so
+		$.ajax({
+			type: "POST", //type post?
+			url: "php/controller/save-user.php", //url for creating users
+			data: {
+				exp: game.data.exp,
+				exp1: game.data.exp1,
+				exp2: game.data.exp2,
+				exp3: game.data.exp3,
+				exp4: game.data.exp4,
+			},
+			//sets username and password to entered info
+			dataType: "text"
+		})
+		.success(function(response) {
+			if(response === "true") {
+				me.state.change(me.state.PLAY);
+				//start game
+			}
+			else {
+				alert(response);
+			}
+		})
+		//if successful 
+		.fail(function(response) {
+			alert("fail");
+		}); //if if fails say so
+		//all this code comes from similar code in index.php
+		//loads game correctly
+
 	}
 });
 //ExperienceManager for player experience
