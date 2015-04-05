169c4db32bc226e88a2a42c177912a703f0ca737
julianinla <vanrietjulian@gmail.com>
2015-04-05 13:54:28 -0700

    Awesomenauts I6 Displaying your Minimap Intermediate

--- js/entities/MiniMap.js
+++ js/entities/MiniMap.js
@@ -1,4 +1,17 @@
 game.MiniMap = me.Entity.extend({
+	init: function(x, y, settings) {
+		this._super(me.Entity, "init", [x, y, (
+			image: "minimap",
+			width: 384,
+			height: 122,
+			spritewidth: "384",
+			spriteheight: "122",
+			getShape: function() {
+				return (new me.Rect(0, 0, 384, 122)).toPolygon();
+			}
+		)]);
 
+		this.floating = true;
+	}
 });
 //basic entity setup for minimap
